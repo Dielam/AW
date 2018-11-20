e36650115c34ec64cdf69a7223583d5e01d5fb0b
Dielam <dielagun@ucm.es>
2018-11-20 20:08:51 +0100

    Arrancar e insertar funcionando!!!!
    Funcionando el mostrar las tareas de la BD.
    Funcionando el insertar tareas en la BD.
    
    Falta marcar las tarea a done y eliminar las tareas completadas.

--- Practica0/app.js
+++ Practica0/app.js
@@ -1,6 +1,7 @@
 
 const config = require("./config");
 const DAOTasks = require("./DAOTasks");
+const tareas = require("./tareas.js");
 const path = require("path");
 const mysql = require("mysql");
 const express = require("express");
@@ -16,25 +17,39 @@ app.set("view engine", "ejs");
 // Definir el directorio de plantillas
 app.set("views", path.join(__dirname, "public", "views"));
 
+// Crear middleware static
+const staticFiles = path.join(__dirname, "public");
+
+// Arrancar middleware static
+app.use(express.static(staticFiles));
+
+// Arrancar body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // Crear el pool de conexiones
 const pool = mysql.createPool(config.mysqlConfig);
 
 // Crear instancia DAOTask
 const daoT = new DAOTasks(pool);
 
-// Getter de la vista tasks.ejs
+// GET de la vista tasks.ejs
 app.get("/tasks", function(request, response){
     response.status(200);
-    daoT.getAllTasks("usuario@ucm.es", function(err, arrayTasks){
-        response.render("tasks", arrayTasks); 
+    daoT.getAllTasks("usuario@ucm.es", function(err, tasksList){
+        //if(err)
+        response.render("tasks", {"tasksList": tasksList}); 
     });
-})
+});
 
-// Crear middleware static
-const static = path.join(__dirname, "public");
-
-// Arrancar middleware static
-app.use(express.static(static));
+// POST del formulario de añadir tareas de la vista tasks.ejs
+app.post("/addTask", function(request, response){
+    response.status(200); 
+    daoT.insertTask("usuario@ucm.es", tareas.createTask(request.body.new_task), function(err){
+        console.log(err);
+        //if(err)
+        response.redirect("/tasks");
+    });
+});
 
 // Arrancar servidor
 app.listen(config.port, function(err){
--- Practica0/tareas.js
+++ Practica0/tareas.js
@@ -3,34 +3,37 @@
 * Diego Laguna Martín
 */
 
-"use strict"; //Se usa para que se tengan que declarar las variables
+module.exports = {
 
-function getToDoTasks(tasks){
-    let toDoTasks = tasks.filter(n => n.done == null || n.done == false).map(n => n.text);
-    return toDoTasks;
+    getToDoTasks: function getToDoTasks(tasks){
+        let toDoTasks = tasks.filter(n => n.done == null || n.done == false).map(n => n.text);
+        return toDoTasks;
+    },
+
+    findByTag: function findByTag(tasks, tag){
+        let tasksWithTag = tasks.filter(n => n.tags.includes(tag));
+        return tasksWithTag;
+    },
+
+    findByTags: function findByTags(tasks, tags){
+        let tasksWithTag = tasks.filter(function (n){ 
+            return tags.some(m => n.tags.includes(m)); 
+            });
+        return tasksWithTag;
+    },
+
+    countDone: function countDone(tasks){
+        let toDoTasks = tasks.filter(n => n.done == true);
+        return toDoTasks.length;
+    },
+
+    createTask: function createTask(texto){
+        let task = {};
+        let array = texto.split(" ");
+        task.text = array.filter(n => n.indexOf("@") == -1).join(" ");
+        task.tags = array.filter(n => n.indexOf("@") >= 0).map(n => n.slice(1));
+        task.done = 0;
+        return task;
+    }
 }
 
-function findByTag(tasks, tag){
-    let tasksWithTag = tasks.filter(n => n.tags.includes(tag));
-    return tasksWithTag;
-}
-
-function findByTags(tasks, tags){
-    let tasksWithTag = tasks.filter(function (n){ 
-        return tags.some(m => n.tags.includes(m)); 
-    	});
-    return tasksWithTag;
-}
-
-function countDone(tasks){
-    let toDoTasks = tasks.filter(n => n.done == true);
-    return toDoTasks.length;
-}
-
-function createTask(texto){
-    let task = {};
-    let array = texto.split(" ");
-    task.text = array.filter(n => n.indexOf("@") == -1).join(" ");
-    task.tags = array.filter(n => n.indexOf("@") >= 0).map(n => n.slice(1));
-    return task;
-}
