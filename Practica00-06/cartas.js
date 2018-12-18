147d942ad405defbd1971cd64d017708840225fe
XarlyCj <carloj03@ucm.es>
2018-12-18 10:44:06 +0100

    Ficheros iniciales y primeros avances
    La base del html y un poquitito de js

---
+++ Practica00-06/cartas.js
@@ -0,0 +1,11 @@
+<script>
+    $(function() {
+        $(".header .header_options .btn").click(function(){
+            
+        })
+    })
+
+    function inicioJuego(dificultad){
+
+    }
+</script>
--- Practica00-06/test.js
+++
@@ -1,2 +0,0 @@
-"use strict"; //Se usa para que se tengan que declarar las variables
-
